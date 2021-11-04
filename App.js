da572ee857a5ce4ca1b169d5efaae923d2936bc4
tonyYZL <t0965176327@gmail.com>
2021-11-04 19:06:04 +0800

    Write self introduction

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>哈哈哈</Text>
+      <Text>哈哈哈哈</Text>
       <StatusBar style="auto" />
     </View>
   );
